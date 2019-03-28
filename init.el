713d3b72d03100168c8d203b4a96bb67b89ae532
gensfl16 <gensfl16@users.noreply.github.com>
2019-03-28 22:13:13 +0800

    enable ggtags

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
 (require 'init-yasnippet)
 ;; (require 'init-window-numbering)
 (require 'init-multiple-cursors)
-;; (require 'init-ggtags)
+(require 'init-ggtags)
 ;; (require 'init-im)
 ;; (require 'init-lsp)
 ;; (require 'init-eglot)
