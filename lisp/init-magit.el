2a2f87a5b6ee0d6108175f21a7c4a6294866d7f6
gensfl16 <gensfl16@users.noreply.github.com>
2019-08-10 21:13:03 +0800

    deinit magit

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,3 +1,14 @@
+;; ref lazycat
+
 (require 'magit)
+
+;; (defvar one-key-menu-magit-alist nil
+;;   "The `one-key' menu alist for MAGIT.")
+;; 
+;; (setq one-key-menu-magit-alist
+;;       '(
+;; 	(("s" . "Magit status") . magit-status+)
+;; 	(("c" . "Magit checkout") . magit-checkout)
+;; 	))
 
 (provide 'init-magit)
